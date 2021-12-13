086f266665eece3c3f9d3fafb04513dffd443963
ccffdead <ccffdead@gmail.com>
2021-12-12 21:21:55 -0500

    added description and TOC to generateMarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,11 +1,40 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// Create a function that returns a license badge based on which license is passed in
+
 function renderLicenseBadge(license) {
   if(license) {
     return `![${license}](https://img.shields.io/badge/license-${license.split(' ').join('%20')}-green);`
   } else {
+   
+    // If there is no license, return an empty string
+   
     return '';
   }
+};
+
+// Creates a function that returns a description
+
+const renderDescription = (title, disc, link) => {
+  if (link) {
+    return `${disc}
+    
+  View the application at [${title}](${link}).`;
+  } else {
+    return `${disc}`;
+  }
+
+};
+
+// Creates a function that returns TOC
+
+const renderToc = contentsArr => {
+  let dataList = '';
+  contentsArr.forEach((item)=> {
+    if (item.content) {
+      dataList += `* [${item.header}](#${(item.header).toLowerCase().split(' ').join('-')})
+`;
+    }
+  });
+  return dataList;
 };
 
 

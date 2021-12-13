cf461702e3a20872a7b6310e0f3f0e554224a09a
ccffdead <ccffdead@gmail.com>
2021-12-12 21:40:50 -0500

    function created to generate markdown for readme

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,12 +1,12 @@
 // Create a function that returns a license badge based on which license is passed in
 
 function renderLicenseBadge(license) {
-  if(license) {
+  if (license) {
     return `![${license}](https://img.shields.io/badge/license-${license.split(' ').join('%20')}-green);`
   } else {
-   
+
     // If there is no license, return an empty string
-   
+
     return '';
   }
 };
@@ -28,7 +28,7 @@ const renderDescription = (title, disc, link) => {
 
 const renderToc = contentsArr => {
   let dataList = '';
-  contentsArr.forEach((item)=> {
+  contentsArr.forEach((item) => {
     if (item.content) {
       dataList += `* [${item.header}](#${(item.header).toLowerCase().split(' ').join('-')})
 `;
@@ -40,11 +40,11 @@ const renderToc = contentsArr => {
 // TODO: Create a function that returns the license link
 
 function renderLicenseLink(license) {
-  if(license) {
+  if (license) {
     return `This is licensed under ${license} license.`;
   } else {
-    
-  // If there is no license, return an empty string
+
+    // If there is no license, return an empty string
 
     return '';
   }
@@ -55,7 +55,7 @@ function renderLicenseLink(license) {
 const renderBuilt = built => {
   let all = '';
 
-  if(built) {
+  if (built) {
     built.forEach(item => {
       all += ` *${item}`
     });
@@ -74,7 +74,7 @@ const renderUse = (use) => {
 //Creates a function that returns how to test the app
 
 const renderTest = test => {
-  if(test) {
+  if (test) {
     return `To use our applictaion install \`\`\` ${test} \`\`\` and run \`node index.js\` from your terminal.`
   } else {
     return '';
@@ -90,3 +90,63 @@ const renderContact = (email, github, repository) => {
   }
 };
 
+//Creates a function to generate markdown for README
+
+function generateMarkdown(data) {
+  const { title, github, repository, license } = data;
+  let rmContents = '';
+  const sectionArr = [
+    {
+      header: 'License',
+      content: renderLicense(license)
+    },
+    {
+      header: 'Installation',
+      content: renderInstall(data.install)
+    },
+    {
+      header: 'Use',
+      content: renderUse(data.use)
+    },
+    {
+      header: 'Built With',
+      content: renderBuilt(data['built'])
+    },
+    {
+      header: 'Contact',
+      content: renderContact(data.contact, repository, github)
+    },
+    {
+      header: 'Test',
+      content: renderTest(data.test)
+    },
+    {
+      header: 'Contribution',
+      content: data.contribution
+    },
+  ];
+
+  //Forms Content Section
+
+  sectionArr.forEach((sectionData) => {
+    if (sectionData.content && sectionData.header === 'Table of Contents') {
+      readmeData += `### ${sectionData.header}${sectionData.content}`;
+    } else if (sectionData.content) {
+      rmContents += `## ${sectionData.header}${sectionData.content}`;
+    }
+  })
+
+  return `# ${title}
+[![Issues](https://img.shields.io/github/issues/${github}/${repository
+    })](https://github.com/${github}/${repository
+    }/issues) [![Issues](https://img.shields.io/github/contributors/${github
+    }/${repository})](https://github.com/${github}/${repository
+    }/graphs/contributors) ${renderLicenseBadge(license)}
+## Description
+${renderDescription(title, data.disc, data.link)}
+## Contents
+${renderToc(sectionArr)}
+${rmContents}`;
+}
+
+module.exports = generateMarkdown;
